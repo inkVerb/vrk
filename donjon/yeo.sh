79b73624382a82c3e83b09e0ae1a0bebb55d7047
jessesteele <jessesteele@gmail.com>
2023-08-22 12:56:26 +0800

    Added yeo

---
+++ donjon/yeo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#inkVerbInk! verb.ink
+
+# This passes on all arguments verbatim to yay, run by the "worker" user
+## The purpose is to ensure that the task is always run by a non-root user, but doesn't prompt for passwords
+
+# This requires normal users to run as "sudo"
+
+# Usage: run "yeo" instead of "yay", otherwise as you normally would
+
+# If root or sudo
+if [ "$(id -u)" != "0" ]; then
+  /usr/bin/echo "Must run as root or sudo!"
+  exit 1
+fi
+
+# Check for quotes
+/usr/bin/echo $@ | grep -q '"' 
+if [ "$?" != "0" ]; then
+  /usr/bin/echo $@ | grep -q "'" 
+  if [ "$?" != "0" ]; then
+    su worker -c "/usr/bin/yay $@"
+    exit $?
+  else
+    /usr/bin/echo "No 'quotes' allowed!"
+    exit 1
+  fi
+else
+  /usr/bin/echo "No \"quotes\" allowed!"
+  exit 1
+fi
